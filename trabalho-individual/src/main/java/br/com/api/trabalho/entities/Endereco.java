10333c6a96dea2b81dd85e3c9220b6c1c311dac2
VictorSoares-C <victorsoares.c@gmail.com>
2023-10-18 15:43:25 -0300

    Feat: Criação do código da entidade endereço

--- trabalho-individual/src/main/java/br/com/api/trabalho/entities/Endereco.java
+++ trabalho-individual/src/main/java/br/com/api/trabalho/entities/Endereco.java
@@ -1,5 +1,134 @@
 package br.com.api.trabalho.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "endereco")
 public class Endereco {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_endereco")
+	private Integer id;
+
+	@NotNull
+	@Column(name = "cep")
+	private String cep;
+
+	@NotNull
+	@Column(name = "logradouro")
+	private String logradouro;
+
+	@NotNull
+	@Column(name = "numero")
+	private String numero;
+
+	@NotNull
+	@Column(name = "novo_complemento")
+	private String complemento;
+
+	@NotNull
+	@Column(name = "bairro")
+	private String bairro;
+
+	@NotNull
+	@Column(name = "localidade")
+	private String localidade;
+
+	@NotNull
+	@Column(name = "uf")
+	private String uf;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public Endereco() {
+	}
+
+	public Endereco(Integer id, @NotNull String cep, @NotNull String logradouro, @NotNull String numero,
+			@NotNull String complemento, @NotNull String bairro, @NotNull String localidade, @NotNull String uf) {
+		this.id = id;
+		this.cep = cep;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.localidade = localidade;
+		this.uf = uf;
+	}
+
+	@Override
+	public String toString() {
+		return "Endereco [id=" + id + ", cep=" + cep + ", logradouro=" + logradouro + ", numero=" + numero
+				+ ", complemento=" + complemento + ", bairro=" + bairro + ", localidade=" + localidade + ", uf=" + uf
+				+ "]";
+	}
 
 }
