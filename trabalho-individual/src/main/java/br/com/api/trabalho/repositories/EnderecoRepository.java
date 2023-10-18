b0accef2733f4574ac0ad709102ae4e1d1b3521c
VictorSoares-C <victorsoares.c@gmail.com>
2023-10-18 15:53:29 -0300

    Feat: Criação do repositório Endereço e atualização do código dos repositórios Carro, Pessoa e Endereço

--- trabalho-individual/src/main/java/br/com/api/trabalho/repositories/EnderecoRepository.java
+++ trabalho-individual/src/main/java/br/com/api/trabalho/repositories/EnderecoRepository.java
@@ -3,9 +3,9 @@ package br.com.api.trabalho.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.api.trabalho.entities.Carro;
+import br.com.api.trabalho.entities.Endereco;
 
 @Repository
-public interface CarroRepository extends JpaRepository<Carro, Integer>{
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
 
 }
--- trabalho-individual/src/main/java/br/com/api/trabalho/repositories/PessoaRepository.java
+++ trabalho-individual/src/main/java/br/com/api/trabalho/repositories/PessoaRepository.java
@@ -1,5 +1,11 @@
 package br.com.api.trabalho.repositories;
 
-public class PessoaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.api.trabalho.entities.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
 
 }
