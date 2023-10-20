2a9758ee3910eff81eeab2b4d081f9d1859fc187
VictorSoares-C <victorsoares.c@gmail.com>
2023-10-20 17:24:10 -0300

    Bugs: Atualização das classes Security Config e Swagger Config para configuração de segurança

--- trabalho-individual/src/main/java/br/com/api/trabalho/config/SwaggerConfig.java
+++ trabalho-individual/src/main/java/br/com/api/trabalho/config/SwaggerConfig.java
@@ -1,5 +1,62 @@
 package br.com.api.trabalho.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
 public class SwaggerConfig {
 
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+	
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+	
+	private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> securityReferences = new ArrayList<>();
+        securityReferences.add(new SecurityReference("JWT", new AuthorizationScope[0]));
+        return securityReferences;
+    }
+	
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+				.securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.api.g5.controllers"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("Documentação Projeto E-Commerce Tradicional")
+                .description("Projeto grupo 5")
+                .license("Apache license version 2.0")
+                .version("8.2.3")
+                .build();
+        return apiInfo;
+    }
 }
